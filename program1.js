0909f1b4ce2e8fe0f664328343a7867ed8d7b983
Sheikh-Sahil0 <sheikhsahilss2429@gmail.com>
2024-02-22 13:46:12 +0530

    Changed by Shaikh Sahil for Inern

--- program1.js
+++ program1.js
@@ -3,7 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const mapping = {
+        ')' : '(',
+        '}' : '{',
+        ']' : ']'
+    };
+
+    for (let char of s) {
+        if (char in mapping) {
+            const topElment = stack.length === 0 ? '#' : stack.pop();
+            if (mapping [char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanToIntMap = {
+        'i' : 1,
+        'v' : 5,
+        'x' : 10,
+        'l' : 50,
+        'c' : 100,
+        'e' : 500,
+        'd' : 1000
+    };
+
+    let total = 0;
+    for (let i  = 0; i < s.length; i++) {
+        const currentvalue = romanToIntMap[s[i]];
+        if (i < s.length-1 && romanToIntMap[s[i]] < romanToIntMap[s[i+1]]) {
+            total -= currentvalue;
+        } else{
+            total += currentvalue;
+        }
+    }
+
+    return total;
 };
 
 
